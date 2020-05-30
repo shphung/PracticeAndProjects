686a3b706d8a6d358755c7ba8d0c7c38aad5c2ca
Steven <stevenphunghuynh@yahoo.com>
2020-05-29 23:07:16 -0700

    #202. Happy Number & #204. Count Primes

---
+++ src/CountPrimes.java
@@ -0,0 +1,45 @@
+/*
+ * Leetcode Practice: #204. Count Primes
+ * 
+ * Count the number of prime numbers less than a non-negative number, n.
+ * 
+ * Example:
+ * 	Input = 10
+ *	Output: 4
+ *	Explanation: There are 4 prime numbers less than 10, they are:
+ *		2, 3, 5, 7.
+ */
+public class CountPrimes {
+
+	public static void main(String[] args) {
+		System.out.println(countPrimes(10));
+
+		System.out.println(countPrimes(2));
+		
+		System.out.println(countPrimes(10000));
+		
+		System.out.println(countPrimes(499979));
+	}
+
+	public static int countPrimes(int n) {
+		int numOfPrimes = 0;
+		for(int i = 2; i < n; i++) {
+			if(isPrime(i)) {
+				numOfPrimes++;
+			}
+		}
+		return numOfPrimes;
+	}
+	
+	public static boolean isPrime(int n) {
+		if(n <= 1) {
+			return false;
+		}
+		for(int i = 2; i < n; i++) {
+			if(n % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
