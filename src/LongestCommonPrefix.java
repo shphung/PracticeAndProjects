bb081052a487d40b95177be3eeda4e7d5a5f3d3c
Steven <stevenphunghuynh@yahoo.com>
2020-05-30 03:27:27 -0700

    #14. Longest Common Prefix
    Write a function to find the longest common prefix string amongst an array of strings. If there is no prefix, return an empty string "".

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Leetcode Practice: #14. Longest Common Prefix
+ * 
+ * Write a function to find the longest common prefix string amongst
+ * an array of strings.
+ * 
+ * If there is no prefix, return an empty string "".
+ * 
+ * Example 1:
+ * 	Input: ["flower", "flow", "flight"]
+ * 	Output: "fl"
+ * 
+ * Example 2:
+ * 	Input: ["dog", "racecar", "car"]
+ * 	Output: ""
+ * 	Explanation: There is no common prefix among the input strings.
+ * 
+ * Note:
+ *  - All given inputs are in lowercase letters a-z.
+ */
+public class LongestCommonPrefix {
+	
+	public static void main(String[] args) {
+		System.out.println(longestCommonPrefix(new String[] {"flower", "flow", "flight"}));
+
+		System.out.println(longestCommonPrefix(new String[] {"dog", "racecar", "car"}));
+		
+		System.out.println(longestCommonPrefix(new String[] {"aca", "cba"}));
+		
+		System.out.println(longestCommonPrefix(new String[] {"abca", "aba", "aaab"}));
+	}
+	
+	public static String longestCommonPrefix(String[] strs) {
+		if(strs.length == 0)
+			return "";
+
+		String s = "";
+		int minLength = Integer.MAX_VALUE;
+		for(int i = 0; i < strs.length; i++) {
+			minLength = Math.min(minLength, strs[i].length());
+		}
+		List<String> list = new ArrayList<String>();
+		for(int currentLetter = 0; currentLetter < minLength; currentLetter++) {
+			for(int currentWord = 0; currentWord < strs.length; currentWord++) {
+				String letter = String.valueOf(strs[currentWord].charAt(currentLetter));
+				if(currentWord == 0) {
+					list.add(letter);
+				} else if(currentWord != 0 && (!list.contains(letter) || !list.get(list.size() - 1).equals(letter))) {
+					list.remove(list.size() - 1);
+					break;
+				}
+			}
+			if(list.isEmpty()) {
+				break;
+			}
+		}
+		for(int i = 0; i < list.size(); i++) {
+			s += list.get(i);
+		}
+		return s;
+	}
+}
