5472a38b37443f5777ebd264e7615953ef3cd162
Steven <stevenphunghuynh@yahoo.com>
2020-05-30 03:34:18 -0700

    Update LongestCommonPrefix.java

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -38,25 +38,36 @@ public class LongestCommonPrefix {
 			return "";
 
 		String s = "";
+		
+		//minLength = smallest string size
+		//Since prefix size will be at most size of smallest string, allows to exit early
 		int minLength = Integer.MAX_VALUE;
 		for(int i = 0; i < strs.length; i++) {
 			minLength = Math.min(minLength, strs[i].length());
 		}
+		
 		List<String> list = new ArrayList<String>();
+		//Check letter of each word before moving onto next letter
 		for(int currentLetter = 0; currentLetter < minLength; currentLetter++) {
 			for(int currentWord = 0; currentWord < strs.length; currentWord++) {
+				//Get word's current char
 				String letter = String.valueOf(strs[currentWord].charAt(currentLetter));
+				//In first iteration of word, always add letter
 				if(currentWord == 0) {
 					list.add(letter);
+				//In other iterations, if list does not have the letter or if letter added in word[0]
+				//does not appear in any of the other words[i], then remove the letter
 				} else if(currentWord != 0 && (!list.contains(letter) || !list.get(list.size() - 1).equals(letter))) {
 					list.remove(list.size() - 1);
 					break;
 				}
 			}
+			//If the beginning letter does not match exit loop, no need to keep checking
 			if(list.isEmpty()) {
 				break;
 			}
 		}
+		//Add list's values to final string
 		for(int i = 0; i < list.size(); i++) {
 			s += list.get(i);
 		}
