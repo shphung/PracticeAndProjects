9025985c1fdab3153e492b850f4ae72e9f21add1
Steven <stevenphunghuynh@yahoo.com>
2020-06-07 03:34:25 -0700

    #461. Hamming Distance
    The Hamming distance between two integers is the number of positions at which the corresponding bits are different.

---
+++ src/BitManipulation/HammingDistance.java
@@ -0,0 +1,29 @@
+package BitManipulation;
+/*
+ * Leetcode Practice: #461. Hamming Distance
+ * 
+ * The Hamming distance between two integers is the number of positions
+ * at which the corresponding bits are different.
+ * 
+ * Given two integers x and y, calculate the Hamming distance.
+ *
+ * Example:
+ * 	Input: x = 1, y = 4
+ *  Output: 2
+ *  Explanation:
+ *	 1   (0 0 0 1)
+ *	 4   (0 1 0 0)
+ *      	↑   ↑
+ *  The above arrows point to positions where the corresponding bits are different.
+ */
+public class HammingDistance {
+
+	public static void main(String[] args) {
+		System.out.println(hammingDistance(1, 4));
+	}
+
+	public static int hammingDistance(int x, int y) {
+		int xor = x ^ y;
+		return Integer.bitCount(xor);
+	}
+}
