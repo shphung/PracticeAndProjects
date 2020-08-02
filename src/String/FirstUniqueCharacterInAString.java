163ba2e23c6cb79bc2b4f56bae697f83e24d1800
Steven <stevenphunghuynh@yahoo.com>
2020-08-02 16:34:12 -0700

    Update FirstUniqueCharacterInAString.java

--- src/String/FirstUniqueCharacterInAString.java
+++ src/String/FirstUniqueCharacterInAString.java
@@ -1,5 +1,6 @@
 package String;
-import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /*
  * Leetcode Practice: #387. First Unique Character in a String
@@ -26,19 +27,41 @@ public class FirstUniqueCharacterInAString {
 	}
 
 	public static int firstUniqChar(String s) {
-		HashMap<Character, Integer> count = new HashMap<Character, Integer>();
-		int n = s.length();
-		
-		for(int i = 0; i < n; i++) {
-			char c = s.charAt(i);
-			count.put(c, count.getOrDefault(c, 0) + 1);
-		}
-		
-		for(int i = 0; i < n; i++) {
-			if(count.get(s.charAt(i)) == 1) {
-				return i;
-			}
-		}
-		return -1;
-	}
+		//All 1 character strings are unique.
+        if(s.length() == 1) {
+            return 0;
+        }
+        
+        Set<Character> set = new HashSet<Character>();
+        
+        //Add all repeating characters to a set.
+        for(int i = 0; i < s.length() - 1; i++) {
+        	//Skip to next i if already found
+            if(set.contains(s.charAt(i))) {
+                continue;
+            }
+            for(int j = i + 1; j < s.length(); j++) {
+            	//Skip to next j if already found
+                if(set.contains(s.charAt(j))) {
+                    continue;
+                }
+                if(s.charAt(i) == s.charAt(j)) {
+                    set.add(s.charAt(i));
+                    continue;
+                }
+            }
+        }
+        
+        //All repeating characters are in set.
+        //If character is not in set, it is a non-repeating character.
+        //Return first non-repeating character.
+        for(int i = 0; i < s.length(); i++) {
+            if(!set.contains(s.charAt(i))) {
+                return i;
+            }
+        }
+        
+        //Return -1 if all characters repeat at least once.
+        return -1;
+    }
 }
